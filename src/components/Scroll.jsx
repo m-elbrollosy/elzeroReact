44fbb904e4be232965c481f654e7c6c2d17c6e72
m-elbrollosy <mahmoud.s.elbrolosy@gmail.com>
2025-11-22 13:26:32 +0200

    scroll component

---
+++ src/components/Scroll.jsx
@@ -0,0 +1,9 @@
+function Scroll () {
+return
+      <div class="scroll">
+        {/*start scroll to top */}
+        <i class="fa-solid fa-circle-chevron-up"></i>
+        {/*end scroll to top */}
+      </div>
+}
+export default Scroll;
